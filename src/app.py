efb54c0df803392ea5f5e4f77e8bd9e492503bf1
Mustafa Al-tekreeti <mmaj1976@gmail.com>
2020-08-25 15:31:45 -0400

    bug: adding the necessary logic to ensure correct entered absolute paths

--- src/app.py
+++ src/app.py
@@ -4,7 +4,12 @@ from os import listdir
 """
     The code expect that the pdf files are prefixed with numeric values to force the required order of merging, for example, the file doc_file.pdf should be renamed as 2-doc_file.pdf if it is required to merge it secondly.
 """
+
 filesDir = input("Please, input the absolute path of the pdf files' directory:")
+
+# make sure that the entered path ends with /
+if filesDir[-1] != '/':
+    filesDir += '/'
 
 mergeList = []
 

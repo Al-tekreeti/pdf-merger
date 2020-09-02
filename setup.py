9177b9b032d84dce26679f9b1163c21a7237bbec
Mustafa Al-tekreeti <mmaj1976@gmail.com>
2020-09-02 12:03:53 -0400

    feat: updating the setup.py file

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setuptools, find_packages
 
+setup(
+    name="pdf-merger",
+    version="0.0.1",
+    description="A tool to merge pdf files",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix-like OSs",
+    ],
+)
